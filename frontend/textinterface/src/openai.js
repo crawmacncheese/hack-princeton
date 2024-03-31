62c71f6c7a11391d236d655e7a82b49aced247f0
Alvin Liu <alvinliu@dynamic-oit-visitornet101-10-25-244-188.princeton.org>
2024-03-30 22:16:37 -0400

    fix

--- frontend/textinterface/src/openai.js
+++ frontend/textinterface/src/openai.js
@@ -15,9 +15,8 @@
 //     return res.data.choices[0].text;
 // }
 import OpenAI from "openai";
-require("dotenv").config();
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, dangerouslyAllowBrowser: true });
+const openai = new OpenAI({ apiKey: "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", dangerouslyAllowBrowser: true });
 
 async function main() {
   const completion = await openai.chat.completions.create({
@@ -27,3 +26,16 @@ async function main() {
 
   console.log(completion.choices[0]);
 }
+
+export async function sendMsg(message) {
+        const res = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: message,
+            temperature: 0.7,
+            max_token: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presense_penalty: 0,
+        });
+        return res.data.choices[0].text;
+    }
